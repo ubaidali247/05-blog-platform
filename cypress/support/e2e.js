654ff162faaab7bb28230b1c736e4a528424dc7d
Ubaid Ali <A00046299@myTUDublin.ie>
2026-07-21 02:35:29 +0100

    Fix CRUD v3: explicit reset+visit in beforeEach, scoped selectors

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -6,7 +6,3 @@ Cypress.Commands.add('resetDB', () => {
 Cypress.Commands.add('getItems', () => {
   return cy.request('GET', 'http://localhost:3005/api/posts').its('body');
 });
-
-beforeEach(() => {
-  cy.resetDB();
-});
